aee2d94cd688a4f7095d4580ed48049b720c49a0
DFurtado67 <90103285+DFurtado67@users.noreply.github.com>
2026-06-10 00:45:02 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const SUPABASE_URL = "https://ssuqjjiptqdgiwawdshl.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://ssuqjjiptqdgiwawdshl.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_0sHINGTYGwYtzY34ri9zqQ__6Lnr4Qv";
 
 export const configured =
